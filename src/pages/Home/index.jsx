7bb8cfe19bed471c9300ad7baf5162250fd0f811
stephany-c <ster.carolinas@hotmail.com>
2025-03-05 22:28:31 -0300

    desenvolvendo

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,30 +1,49 @@
 import { useState } from 'react'
 import './style.css'
+import Excluir from '../../assets/excluir.png'
 
 function Home() {
-  const [count, setCount] = useState(0)
+  const users = [
+    {
+      id: '2334y34hgdjfgd',
+      name: 'Rodolfo',
+      age: 33,
+      email: 'rodolfo@gmail.com'
+    },
+    {
+      id: '2334y3jdfdjfjfgd',
+      name: 'Aline',
+      age: 33,
+      email: 'aline@gmail.com'
+    }
+  ];
 
   return (
-
-      <div>
+    <div>
       <div className='container'>
-
-          <form>
+        <form>
           <h1>Cadastro de Usuários</h1>
-          <input name='nome' type='text/'></input>
-          <input name='idade' type='number/'></input>
-          <input name='email' type='email/'></input>
+          <input placeholder='Nome'  name='nome' type='text'></input>
+          <input placeholder='Idade' name='idade' type='number'></input>
+          <input placeholder='E-mail' name='email' type='email'></input>
           <button type='button'>Cadastrar</button>
         </form>
-            
-    </div>
-      </div>
-     
-      
 
-      
- 
-  )
+        {users.map((user) => (
+          <div key={user.id} className='card'>
+            <div>
+              <p>Nome: <span>{user.name}</span></p>
+              <p>Idade: <span>{user.age}</span></p>
+              <p>Email: <span>{user.email}</span></p>
+            </div>
+            <button>
+              <img src={Excluir} alt="Excluir usuário"/> 
+            </button>         
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;

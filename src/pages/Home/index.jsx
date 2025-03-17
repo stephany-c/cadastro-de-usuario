6b9c6c18e0b499f7c3dcf5e375a264465d5fff6f
stephany-c <ster.carolinas@hotmail.com>
2025-03-16 22:57:26 -0300

    conectando ao servidor

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,29 +1,26 @@
-import { useState } from 'react'
+
+import { useEffect } from 'react'
 import './style.css'
 import Excluir from '../../assets/excluir.png'
+import api from '../../services/api'
 
 function Home() {
-  const users = [
-    {
-      id: '2334y34hgdjfgd',
-      name: 'Rodolfo',
-      age: 33,
-      email: 'rodolfo@gmail.com'
-    },
-    {
-      id: '2334y3jdfdjfjfgd',
-      name: 'Aline',
-      age: 33,
-      email: 'aline@gmail.com'
-    }
-  ];
+  let users = [];
+  async function getUsers() {
+    const usersFromApi = await api.get('/usuarios')
+    users = usersFromApi.data
+    console.log(users)
+  }
+  useEffect(() => {
+    getUsers()
+  }, [])
 
   return (
     <div>
       <div className='container'>
         <form>
           <h1>Cadastro de Usuários</h1>
-          <input placeholder='Nome'  name='nome' type='text'></input>
+          <input placeholder='Nome' name='nome' type='text'></input>
           <input placeholder='Idade' name='idade' type='number'></input>
           <input placeholder='E-mail' name='email' type='email'></input>
           <button type='button'>Cadastrar</button>
@@ -37,13 +34,12 @@ function Home() {
               <p>Email: <span>{user.email}</span></p>
             </div>
             <button>
-              <img src={Excluir} alt="Excluir usuário"/> 
-            </button>         
+              <img src={Excluir} alt="Excluir usuário" />
+            </button>
           </div>
         ))}
       </div>
     </div>
   );
 }
-
 export default Home;
